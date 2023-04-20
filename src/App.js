c78330eed17cb5030f3c6da79a1b60464e8fdcbc
mlabato <m_labato@hotmail.com>
2023-04-20 12:34:24 -0300

    home page added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import { ChakraProvider} from '@chakra-ui/react'
 import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import theme from './theme';
-
-import '@fontsource/raleway/400.css'
-import '@fontsource/open-sans/700.css'
-
-
+import Home from "./pages/Home";
 
 function App() {
   return (
-   <ChakraProvider theme={theme}>
-    <Routes>
-    <Route path="/" element={<Home/>} />
-    </Routes>
-   </ChakraProvider>
+    <section className="font-jost">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </section>
   );
 }
 
