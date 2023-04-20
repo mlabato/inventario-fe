dfd29c4dc40f5763fc5d53c4159446f3aa3eff64
mlabato <m_labato@hotmail.com>
2023-04-20 14:03:22 -0300

    /add page added

---
+++ src/pages/Add.js
@@ -0,0 +1,15 @@
+import Overlay from "../ui/Overlay";
+import Navbar from "../components/Navbar";
+import AddForm from "../components/AddForm"
+
+const Add = () => {
+    return(
+        <Overlay>
+            <Navbar />
+            <AddForm />
+        </Overlay>
+    )
+
+}
+
+export default Add;
