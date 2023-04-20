65fdc852bf522510de39b7c53430e9d1d31632db
mlabato <m_labato@hotmail.com>
2023-04-20 14:03:36 -0300

    /add page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Add from "./pages/Add";
 
 function App() {
   return (
     <section className="font-jost">
       <Routes>
         <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/agregar" element={<Add/>} />
       </Routes>
     </section>
   );
